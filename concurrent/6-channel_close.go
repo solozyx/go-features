93e299b28ed26e86778ad1250994cc073aa1f688
solozyx <zhaoyingxin@aliyun.com>
2018-12-02 16:36:36 +0800

    channel

--- concurrent/6-channel_close.go
+++ concurrent/6-channel_close.go
@@ -21,7 +21,72 @@ func closeChan() {
 	fmt.Println("n1=", n1)
 }
 
+func sendData(ch1 chan int){
+	for i := 1; i <= 5; i++ {
+		ch1 <- i
+	}
+	fmt.Println("sendData 子协程发送方,写入数据完毕..")
+	// 发送方:关闭通道,用于通知接受方没有数据继续发送了
+	close(ch1)
+}
 
+/*
+main主协程 接收方 读取到数据:  1 true
+main主协程 接收方 读取到数据:  2 true
+main主协程 接收方 读取到数据:  3 true
+main主协程 接收方 读取到数据:  4 true
+main主协程 接收方 读取到数据:  5 true
+sendData 子协程发送方,写入数据完毕..
+main主协程 接收方 读取完毕,发送方把通道关闭了.. ok =  false
+main主协程 接收方 读取到数据:  0 false
+main主协程 接收方 读取完毕,发送方把通道关闭了.. ok =  false
+main主协程 接收方 读取到数据:  0 false
+main主协程 接收方 读取完毕,发送方把通道关闭了.. ok =  false
+main主协程 接收方 读取到数据:  0 false
+main主协程 接收方 读取完毕,发送方把通道关闭了.. ok =  false
+main主协程 接收方 读取到数据:  0 false
+
+Process finished with exit code 2
+*/
+func closeChan1(){
+	ch1 := make(chan int)
+	go sendData(ch1)
+	// main主协程 接收方 读取数据
+	for{
+		time.Sleep(1*time.Second)
+		data ,ok:= <- ch1
+		if !ok{
+			fmt.Println("main主协程 接收方 读取完毕,发送方把通道关闭了.. ok = ",ok)
+		}
+		fmt.Println("main主协程 接收方 读取到数据: ",data,ok)
+	}
+}
+
+/*
+main主协程 接收方 读取到数据:  1 true
+main主协程 接收方 读取到数据:  2 true
+main主协程 接收方 读取到数据:  3 true
+main主协程 接收方 读取到数据:  4 true
+main主协程 接收方 读取到数据:  5 true
+sendData 子协程发送方,写入数据完毕..
+main主协程 接收方 读取完毕,发送方把通道关闭了.. ok =  false
+
+Process finished with exit code 0
+*/
+func closeChan2(){
+	ch1 := make(chan int)
+	go sendData(ch1)
+	// main主协程 接收方 读取数据
+	for{
+		time.Sleep(1*time.Second)
+		data ,ok:= <- ch1
+		if !ok{
+			fmt.Println("main主协程 接收方 读取完毕,发送方把通道关闭了.. ok = ",ok)
+			break
+		}
+		fmt.Println("main主协程 接收方 读取到数据: ",data,ok)
+	}
+}
 
 func iterateChan() {
 	intChan2 := make(chan int, 100)
@@ -43,8 +108,25 @@ func iterateChan() {
 	}
 }
 
+func sendDataString(ch1 chan  string){
+	for i := 1; i <= 10; i++ {
+		ch1 <- fmt.Sprint("数据",i)
+	}
+	close(ch1)
+}
+
+func iterateChan1(){
+	ch1 := make(chan string)
+	go sendDataString(ch1)
+	for value := range ch1 { // 停止条件：通道关闭，显示的调用close()
+		fmt.Println("main主协程 从通道中读取数据: ",value)
+	}
+}
+
 func main() {
 	// closeChan()
-	closeChan1()
+	// closeChan1()
+	// closeChan2()
 	// iterateChan()
+	iterateChan1()
 }
